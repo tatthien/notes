a71dbd4d187d1691b56ea7e36f33aaf269f134be
Nguyễn Tất Thiện <tatthien.contact@gmail.com>
2017-10-04 15:04:06 +0700

    Add highlight.js

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,5 @@ export default {
 
 <style lang="scss">
   @import "assets/scss/main.scss";
+  @import "~highlight.js/styles/monokai-sublime.css"
 </style>
--- src/components/pages/SinglePost.vue
+++ src/components/pages/SinglePost.vue
@@ -22,8 +22,31 @@
 </template>
 
 <script>
-  import marked from 'marked'
+  import marked, { Renderer } from 'marked'
+  import highlightjs from 'highlight.js'
   import VueDisqus from 'vue-disqus/VueDisqus.vue'
+  
+  const renderer = new Renderer()
+  renderer.code = (code, language) => {
+    // Check whether the given language is valid for highlight.js.
+    const validLang = !!(language && highlightjs.getLanguage(language))
+    // Highlight only if the language is valid.
+    const highlighted = validLang ? highlightjs.highlight(language, code).value : code
+    // Render the highlighted code with `hljs` class.
+    return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`
+  }
+
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false
+  })
+
   export default {
     components: {
       VueDisqus
